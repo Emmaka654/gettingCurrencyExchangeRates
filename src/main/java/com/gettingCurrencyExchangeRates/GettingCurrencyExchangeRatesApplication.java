e73e46b57c0f82dd50c10ecfcedf338a72bf1e6c
Emmaka654 <emi-metels@yandex.ru>
2024-07-19 11:36:50 +0300

    Create a basic application.

---
+++ src/main/java/com/gettingCurrencyExchangeRates/GettingCurrencyExchangeRatesApplication.java
@@ -0,0 +1,13 @@
+package com.gettingCurrencyExchangeRates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingCurrencyExchangeRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingCurrencyExchangeRatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gettingCurrencyExchangeRates/GettingCurrencyExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gettingCurrencyExchangeRates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingCurrencyExchangeRatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
