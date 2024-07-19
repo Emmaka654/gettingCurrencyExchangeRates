ed668866799eb31a663e079e979f50929385161d
Emmaka654 <emi-metels@yandex.ru>
2024-07-19 14:14:25 +0300

    Fix errors.

--- src/main/java/com/gettingCurrencyExchangeRates/repository/CurrencyRepository.java
+++ src/main/java/com/gettingCurrencyExchangeRates/repository/CurrencyRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 
+
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
-    boolean loadDataForDate(LocalDate date);
-    Currency getCurrencyByDateAndCode(LocalDate date, String currencyCode);
+
+    Currency getCurrencyByDateAndId(LocalDate date, Long currencyId);
 }
